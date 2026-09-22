d8864dac63cef3170669109e8f6c4616ad8a38c6
Akel <akel@akel.Akel-Debian>
2026-09-22 14:58:05 -0300

    Corrección Terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket     = "classic-library-terraform-state"
-    key        = "infra/terraform.tfstate"
-    region     = "us-east-1"
-    use_lockfile = true
-    encrypt    = true
+    bucket         = "classic-library-terraform-state"
+    key            = "infra/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "classic-library-terraform-locks"
+    encrypt        = true
   }
 }
